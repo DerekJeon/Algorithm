e26ed9d7fb1e670823f6f23be75a9fda5dc0992d
unknown <dongwon0207@sadcafesk.onmicrosoft.com>
2023-10-01 01:51:00 +0900

    231001-2

---
+++ bj-2745.py
@@ -0,0 +1,13 @@
+init = input()
+number, notation = init.split(" ")
+number_split = list(number)
+numbering = {"0": 0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10,
+             "A":10, "B":11, "C":12, "D":13, "E":14, "F":15, "G":16, "H":17, "I":18, "J":19, "K":20, "L":21,
+             "M":22, "N":23, "O":24, "P":25, "Q":26, "R":27, "S":28, "T":29, "U":30, "V":31, "W":32, "X":33, 
+             "Y":34, "Z":35}
+result = 0
+for a in range(len(number_split)):
+    result = result + (int(notation)**(len(number_split)-a-1))*numbering[number_split[a]]
+
+print(result)
+
